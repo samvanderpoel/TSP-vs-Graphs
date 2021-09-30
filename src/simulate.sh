986c302c833cec9f9f64ff7dd62bad110d264064
Sam van der Poel <samrvanderpoel@gmail.com>
2021-09-30 17:28:20 -0400

    renamed simulation.py to main.py

--- src/simulate.sh
+++ src/simulate.sh
@@ -3,6 +3,6 @@
 PT_CLOUD_TYPES=( pts_uni pts_annulus_random pts_ball pts_clusnorm pts_corners pts_normal pts_spokes )
 
 for type in ${PT_CLOUD_TYPES[@]}; do
-    python src/simulation.py --minpts=10 --maxpts=1360 --interval=50 --numrunsper=50 --batch=50 --randtype="${type}"
+    python src/main.py --minpts=10 --maxpts=1360 --interval=50 --numrunsper=50 --batch=50 --randtype="${type}"
     python src/read_simul_data.py --randtype="${type}"
 done
