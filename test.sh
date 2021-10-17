57245cb33e74b79fbf3e201fad15beeeb0c3bed6
Sam van der Poel <samrvanderpoel@gmail.com>
2021-10-17 11:45:02 -0400

    update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+tour="{'tour':['1nng','2nng','20pt','mst','gab','urq','del','1del','2del','bito','path'],"
+path="'path':['1nng','2nng','20pt','mst','gab','urq','del','1del','2del'],"
+bito="'bito':['1nng','2nng','20pt','mst','gab','urq','del','1del','2del']}"
+comps=$tour$path$bito
+anoms="{}"
+
 mkdir tour-wds
 mkdir path-wds
 
-python src/main.py --minpts=10 --maxpts=110 --interval=50 --numrunsper=50 --batch=50 --randtype=pts_uni
+python src/main.py --minpts=10 --maxpts=60 --interval=10 --numrunsper=20 --batch=20 --randtype=pts_uni --comps=${comps} --anoms=${anoms}
 python src/read_simul_data.py --randtype=pts_uni
 
 rm -r tour-wds
