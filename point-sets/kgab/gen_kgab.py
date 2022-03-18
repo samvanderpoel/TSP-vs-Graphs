4ef328aea50e72c4de5b79ca41dd6e84cdf15004
Sam van der Poel <samrvanderpoel@gmail.com>
2022-03-18 14:35:42 -0400

    reformat point sets dir

--- point-sets/kgab/gen_kgab.py
+++ point-sets/kgab/gen_kgab.py
@@ -5,11 +5,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--n', type=int, required=True)
 n = parser.parse_args().n
 
-fname = 'example-graphs/kgab/gab' + str(n) + '.yaml'
+fname = f"point-sets/kgab/gab{n}.yaml"
 
 with open(fname, 'w') as f:
-    lines = ['points:\n', '  - [0,0]\n']
+    lines = ["points:\n", "  - [0,0]\n"]
     for i in range(1, n-1):
-        lines.append('  - ' + str([5*i,(-1)**i]) + '\n')
-    lines.append('  - ' + str([5*(n-1),0]) + '\n')
+        lines.append(f"  - {[5*i,(-1)**i]}\n")
+    lines.append(f"  - {[5*(n-1),0]}\n")
     f.writelines(lines)
