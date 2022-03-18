a3f4b158f28a423224a81623549fd947e914fe44
Sam van der Poel <samrvanderpoel@gmail.com>
2022-03-18 14:46:09 -0400

    improve plotting features and update accompanying scripts

--- plot/graph_funcs.py
+++ plot/graph_funcs.py
@@ -6,10 +6,10 @@ import numpy as np
 import networkx as nx
 import math
 
-from sklearn.neighbors import NearestNeighbors # for KNNG
-from scipy.spatial import Delaunay # for Delaunay and Urquhart
-from scipy.spatial import Voronoi # for Gabriel
-from concorde.tsp import TSPSolver # for solve_tsp_from_file
+from sklearn.neighbors import NearestNeighbors
+from scipy.spatial import Delaunay
+from scipy.spatial import Voronoi
+from concorde.tsp import TSPSolver
 from orderk_delaunay import *
 
 def ccw(A,B,C):
@@ -19,18 +19,18 @@ def intersect(A,B,C,D):
     return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
 def perturb_pts(points, xptb=0.001, yptb=0.001):
-    """ For randomly perturbing (uniformly) the x coordinate and ycoordinate of points
-    within an interval [x-xptb, x+xptb] and [y-yptb,y+yptb] for all points
-    Useful if we want to tweak an underlying point-distribution to remove degeneracies
+    """
+    Perturb (uniformly) the x- and y-coordinates of points
+    within intervals [x-xptb, x+xptb], [y-yptb,y+yptb]
+    Useful for removing degeneracies
     """
     points = np.asarray(points)
-    dxs    = -xptb + 2*xptb*np.random.random(len(points))
-    dys    = -yptb + 2*yptb*np.random.random(len(points))
-    dps    = np.asarray(list(zip(dxs,dys)))
+    dxs = -xptb + 2*xptb*np.random.random(len(points))
+    dys = -yptb + 2*yptb*np.random.random(len(points))
+    dps = np.asarray(list(zip(dxs,dys)))
     points = points + dps
     return points
 
-#### NNG ####
 def get_nng_graph(points, k=None, pct=None, metric=2):
     n = len(points)
     if pct is not None:
@@ -40,14 +40,18 @@ def get_nng_graph(points, k=None, pct=None, metric=2):
     nng_graph = nx.Graph()
     nng_graph.add_nodes_from(zip(range(len(points)), coords))
     if metric=='inf':
-        nbrs = NearestNeighbors(n_neighbors=(k+1),
-                                algorithm='ball_tree',
-                                metric='chebyshev').fit(points)
+        nbrs = NearestNeighbors(
+            n_neighbors=(k+1),
+            algorithm='ball_tree',
+            metric='chebyshev'
+        ).fit(points)
     else:
-        nbrs = NearestNeighbors(n_neighbors=(k+1),
-                                algorithm='ball_tree',
-                                metric='minkowski',
-                                p=float(metric)).fit(points)
+        nbrs = NearestNeighbors(
+            n_neighbors=(k+1),
+            algorithm='ball_tree',
+            metric='minkowski',
+            p=float(metric)
+        ).fit(points)
     distances, indices = nbrs.kneighbors(points)
     edge_list = []
 
@@ -57,15 +61,11 @@ def get_nng_graph(points, k=None, pct=None, metric=2):
 
     nng_graph.add_edges_from(edge_list)
     if pct is not None:
-        nng_graph.graph['type']   = str(100*pct)+'-pct-nng'
-        nng_graph.graph['weight'] =  None # TODO, also edge weights for each edge!!!
-        print("Finished computing " + str(100*pct) + "-percent NNG, numpts=" + str(len(points)) + ", iteration " + str(iteration))
+        nng_graph.graph['type'] = f"{100*pct}-pct-nng"
     else:
-        nng_graph.graph['type']   = 'nng'
-        nng_graph.graph['weight'] =  None # TODO, also edge weights for each edge!!!
+        nng_graph.graph['type'] = 'nng'
     return nng_graph
 
-#### Delaunay Triangulation ####
 def get_delaunay_tri_graph(points):
     points       = np.array(points)
     coords       = [{"pos":pt} for pt in points]
@@ -88,15 +88,14 @@ def get_delaunay_tri_graph(points):
         deltri_graph.edges[n1,n2]['weight'] = edge_wt
         total_weight_of_edges = total_weight_of_edges + edge_wt 
     deltri_graph.graph['weight'] = total_weight_of_edges
-    deltri_graph.graph['type']   = 'dt'
+    deltri_graph.graph['type'] = 'dt'
     
     return deltri_graph
 
-#### MST ####
 def get_mst_graph(points):
     points = np.array(points)
-    coords       = [{"pos":pt} for pt in points]
-    tri          = Delaunay(points)
+    coords = [{"pos":pt} for pt in points]
+    tri = Delaunay(points)
     deltri_graph = nx.Graph()
     deltri_graph.add_nodes_from(zip(range(len(points)), coords))
     edge_list = []
@@ -110,12 +109,19 @@ def get_mst_graph(points):
         edge_wt = np.linalg.norm(pt1-pt2)
         deltri_graph.edges[n1,n2]['weight'] = edge_wt
 
-    mst_graph = nx.algorithms.tree.mst.minimum_spanning_tree(deltri_graph, algorithm='kruskal')
+    mst_graph = (
+        nx
+        .algorithms
+        .tree
+        .mst
+        .minimum_spanning_tree(
+            deltri_graph, algorithm='kruskal'
+        )
+    )
     mst_graph.graph['type'] = 'mst'
     
     return mst_graph
 
-#### Gabriel ####
 def get_gabriel_graph(points):
     points = np.array(points)
     coords = [{"pos":pt} for pt in points]
@@ -139,9 +145,13 @@ def get_gabriel_graph(points):
             normal = np.array([-p1p2[1], p1p2[0]])
 
             midpoint = vor.points[[p1, p2]].mean(axis=0)
-            direction = np.sign(np.dot(midpoint - center, normal)) * normal
-            length = 2*max(np.linalg.norm(vor.points[p1]-vor.vertices[v2]),
-                           np.linalg.norm(vor.points[p2]-vor.vertices[v2]))
+            direction = np.sign(
+                np.dot(midpoint - center, normal)
+            ) * normal
+            length = 2 * max(
+                np.linalg.norm(vor.points[p1]-vor.vertices[v2]),
+                np.linalg.norm(vor.points[p2]-vor.vertices[v2])
+            )
             far_point = vor.vertices[v2] + direction * length
             
             if intersect(vor.points[p1], vor.points[p2],
@@ -153,7 +163,6 @@ def get_gabriel_graph(points):
     
     return gabriel
 
-#### Urquhart ####
 def get_urquhart_graph(points):
     points       = np.array(points)
     coords       = [{"pos":pt} for pt in points]
@@ -189,7 +198,6 @@ def get_urquhart_graph(points):
     
     return urq_graph
 
-#### Bitonic ####
 def get_bitonic_tour(points):
     points = perturb_pts(np.array(points))
     points = sorted(points , key=lambda k: [k[0], k[1]])
@@ -198,21 +206,36 @@ def get_bitonic_tour(points):
     bitonic_tour.add_nodes_from(zip(range(len(points)), coords))
     n = len(points)
     
-    min_lengths = [0, np.linalg.norm(points[0]-points[1])] # records lengths of latest partial path
-    partial_bitonic_path_edges = {1:[[1,0]]} # records partial bitonic paths at each iteration l
+    # records lengths of latest partial path
+    min_lengths = [0, np.linalg.norm(points[0]-points[1])]
+    # records partial bitonic paths at each iteration l
+    partial_bitonic_path_edges = {1:[[1,0]]}
     for l in range(2,n):
         path_values = []
         for i in range(2,l+1):
             # record lengths of candidate paths at the l-th step
-            path_values.append(np.linalg.norm(points[l]-points[i-2]) + \
-                               min_lengths[i-1] + \
-                               sum( [np.linalg.norm(points[k]-points[k-1]) for k in range(i,l)] ) )
+            path_values.append(
+                np.linalg.norm(points[l]-points[i-2]) +
+                min_lengths[i-1] +
+                sum(
+                    [
+                        np.linalg.norm(points[k]-points[k-1])
+                        for k in range(i,l)
+                    ]
+                )
+            )
         # determine length and index of shortest candidate path
-        path_lngth, idx = min((val, idx) for (idx, val) in enumerate(path_values))
+        path_lngth, idx = min(
+            (val, idx)
+            for (idx, val) in enumerate(path_values)
+        )
         min_lengths = min_lengths + [path_lngth]
         # update latest partial bitonic path
-        partial_bitonic_path_edges[l] = partial_bitonic_path_edges[idx+1] + [[l,idx]] + \
-                                        [[k-1,k] for k in range(idx+2,l)]
+        partial_bitonic_path_edges[l] = (
+            partial_bitonic_path_edges[idx+1] +
+            [[l,idx]] +
+            [[k-1,k] for k in range(idx+2,l)]
+        )
     bitonic_tour_edges = partial_bitonic_path_edges[n-1] + [[n-2,n-1]]
     bitonic_tour.add_edges_from(bitonic_tour_edges)
 
@@ -229,52 +252,56 @@ def get_bitonic_tour(points):
     
     return bitonic_tour
 
-### ONION GRAPH ###
 def get_onion_graph(points):
-     from scipy.spatial import ConvexHull
-     points      = np.asarray(points)   
-     points = sorted(points , key=lambda k: [k[0], k[1]])  
-     points_tmp  = points.copy()
-     numpts      = len(points)
-     onion_graph = nx.Graph()
-     numpts_proc = - 1
+    from scipy.spatial import ConvexHull
+    points = np.asarray(points)   
+    points = sorted(points , key=lambda k: [k[0], k[1]])  
+    points_tmp = points.copy()
+    numpts = len(points)
+    onion_graph = nx.Graph()
+    numpts_proc = -1
 
-     def circular_edge_zip(xs):
-         xs = list(xs) # in the event that xs is of the zip or range type 
-         if len(xs) in [0,1] :
-              zipl = []
-         elif len(xs) == 2 :
-              zipl = [(xs[0],xs[1])]
-         else:
-              zipl = list(zip(xs,xs[1:]+xs[:1]))
-         return zipl
+    def circular_edge_zip(xs):
+        xs = list(xs)
+        if len(xs) in [0,1] :
+            zipl = []
+        elif len(xs) == 2 :
+            zipl = [(xs[0],xs[1])]
+        else:
+            zipl = list(zip(xs,xs[1:]+xs[:1]))
+        return zipl
 
-     while len(points_tmp) >= 3:
-           hull            = ConvexHull(points_tmp)
-           pts_on_hull     = [points_tmp[i] for i in hull.vertices]
-           coords          = [{"pos":pt} for pt in pts_on_hull]
-           new_node_idxs   = range(numpts_proc+1, numpts_proc+len(hull.vertices)+1)
-           onion_graph.add_nodes_from(zip(new_node_idxs, coords))
-           onion_graph.add_edges_from(circular_edge_zip(new_node_idxs))
-           numpts_proc  = numpts_proc + len(hull.vertices)
-           rem_pts_idxs = list(set(range(len(points_tmp)))-set(hull.vertices)) 
-           points_tmp   = [ points_tmp[idx] for idx in rem_pts_idxs ]
-           coords       = [{"pos":pt} for pt in points]
+    while len(points_tmp) >= 3:
+        hull = ConvexHull(points_tmp)
+        pts_on_hull = [points_tmp[i] for i in hull.vertices]
+        coords = [{"pos":pt} for pt in pts_on_hull]
+        new_node_idxs = range(
+            numpts_proc + 1,
+            numpts_proc + len(hull.vertices) + 1
+        )
+        onion_graph.add_nodes_from(zip(new_node_idxs, coords))
+        onion_graph.add_edges_from(circular_edge_zip(new_node_idxs))
+        numpts_proc = numpts_proc + len(hull.vertices)
+        rem_pts_idxs = list(
+            set(range(len(points_tmp))) - set(hull.vertices)
+        ) 
+        points_tmp = [ points_tmp[idx] for idx in rem_pts_idxs ]
+        coords = [{"pos":pt} for pt in points]
 
-     if len(points_tmp) == 2:
-          p, l = numpts_proc+1, numpts_proc+2
-          onion_graph.add_node(p)
-          onion_graph.add_node(l)
-          onion_graph.nodes[p]['coods'] = points_tmp[0]
-          onion_graph.nodes[l]['coods'] = points_tmp[1]
-          onion_graph.add_edge(p,l)
-     elif len(points_tmp) == 1:
-          l = numpts_proc+1 
-          onion_graph.add_node(l)
-          onion_graph.nodes[l]['cood'] = points_tmp[0]
+    if len(points_tmp) == 2:
+        p, l = numpts_proc+1, numpts_proc+2
+        onion_graph.add_node(p)
+        onion_graph.add_node(l)
+        onion_graph.nodes[p]['coods'] = points_tmp[0]
+        onion_graph.nodes[l]['coods'] = points_tmp[1]
+        onion_graph.add_edge(p,l)
+    elif len(points_tmp) == 1:
+        l = numpts_proc+1 
+        onion_graph.add_node(l)
+        onion_graph.nodes[l]['cood'] = points_tmp[0]
  
-     onion_graph.graph['type'] = 'onion'
-     return onion_graph
+    onion_graph.graph['type'] = 'onion'
+    return onion_graph
 
 ##### Generate Distance matrix ##### 
 def generate_distance_matrix(pts, metric, mode):
@@ -290,29 +317,32 @@ def generate_distance_matrix(pts, metric, mode):
         D   = np.zeros((N+t,N+t))
         for i in range(N):
             for j in range(N):
-                D[i,j] = np.linalg.norm(pts[i]-pts[j], ord=float(metric))
+                D[i,j] = np.linalg.norm(
+                    pts[i]-pts[j],
+                    ord=float(metric)
+                )
     return D
 
 #### Write distance matrix to file ####
 def write_distance_matrix_to_file(D,fname, dscale = 10000):
     with open(fname, 'w') as file:
         numrows, numcols = D.shape[0], D.shape[1]
-        assert numrows == numcols, "Number of rows and columns in distance matrix must be equal, as matrix of distances is square"
+        assert numrows == numcols, "row length must equal column length"
 
-        file.write('NAME: sampleinstance\n')
-        file.write('TYPE: TSP\n')
-        file.write('COMMENT: An explicit distance matrix between given set of points. Scaling factor (dscale) used for getting integer distance below = {dscale}\n'.format(dscale=dscale))
-        file.write('DIMENSION: {dim}\n'.format(dim=numrows))
-        file.write('EDGE_WEIGHT_TYPE: EXPLICIT\n')
-        file.write('EDGE_WEIGHT_FORMAT: FULL_MATRIX\n')
-        file.write('EDGE_WEIGHT_SECTION\n')
+        file.write(
+            "NAME: sampleinstance\n"
+            "TYPE: TSP\n"
+            f"COMMENT: Distance matrix given; scaling factor {dscale}\n"
+            f"DIMENSION: {numrows}\n"
+            "EDGE_WEIGHT_TYPE: EXPLICIT\n"
+            "EDGE_WEIGHT_FORMAT: FULL_MATRIX\n"
+            "EDGE_WEIGHT_SECTION\n"
+        )
         
-        # it is essential that distances are integers, otherwise concorde crashes. 
-        # this is also the reason for the scaling factor that has been introduced, 
-        # to make sure the original distances between the data are preserved as much as possible
+        # distances must be integers. so scaling factor is used
         for i in range(numrows):
             for j in range(numcols):
-                file.write('{value} \t'.format(value=int(dscale*D[i,j])) )
+                file.write(f'{int(dscale*D[i,j])} \t')
             file.write('\n')
         file.write('EOF')
 
@@ -356,7 +386,11 @@ def get_tsp_graph(points, mode, metric='2', typ=None):
         solution = solver.solve()
     else:
         D = generate_distance_matrix(points, metric=metric, mode=mode)
-        write_distance_matrix_to_file(D,fname='instance.tsp',  dscale = 1000000)
+        write_distance_matrix_to_file(
+            D,
+            fname='instance.tsp',
+            dscale=1000000
+        )
         solution = solve_tsp_from_file('instance.tsp')
 
     # get solution inds and add nodes
@@ -364,16 +398,20 @@ def get_tsp_graph(points, mode, metric='2', typ=None):
 
     # add correct edges to graph
     if mode=='tour':
-        edge_list = list(zip(idxs_along_tsp, idxs_along_tsp[1:])) + \
-                    [(idxs_along_tsp[-1],idxs_along_tsp[0])]
+        edge_list = (
+            list(zip(idxs_along_tsp, idxs_along_tsp[1:])) +
+            [(idxs_along_tsp[-1],idxs_along_tsp[0])]
+        )
         tsp_graph.add_edges_from(edge_list)
     elif mode=='path':
         dummy_node_ind = idxs_along_tsp.index(n)
         if dummy_node_ind == 0:
             path = idxs_along_tsp[1:]
         else:
-            path = idxs_along_tsp[dummy_node_ind+1:] + \
-                   idxs_along_tsp[:dummy_node_ind]
+            path = (
+                idxs_along_tsp[dummy_node_ind+1:] +
+                idxs_along_tsp[:dummy_node_ind]
+            )
         for i in range(0,n-1):
             tsp_graph.add_edge(path[i], path[i+1])
 
@@ -387,9 +425,9 @@ def get_tsp_graph(points, mode, metric='2', typ=None):
         total_weight_of_edges = total_weight_of_edges + edge_wt 
     tsp_graph.graph['weight'] = total_weight_of_edges
     if typ is not None:
-        tsp_graph.graph['type']   = 'poly'
+        tsp_graph.graph['type'] = 'poly'
     else:
-        tsp_graph.graph['type']   = 'concorde'
+        tsp_graph.graph['type'] = 'concorde'
 
     for file in os.listdir(os.getcwd()):
         os.remove(os.path.join(os.getcwd(), file))
@@ -400,22 +438,28 @@ def get_tsp_graph(points, mode, metric='2', typ=None):
 
 #### Order-k Delaunay Graph ####
 def get_kdelaunay_graph(points, order):
-    """Returns the k-Delaunay graph.
-    The k-Delaunay graph (a.k.a., the order k Delaunay graph) contains an edge
-    ij if and only if there exists a disk containing i and j and at most k
-    additional points. See that the 0-Delaunay graph is the standard Delaunay
-    graph. Moreover, for any natural k, k-Delaunay \subseteq (k+1)-Delaunay.
+    """
+    Returns the k-Delaunay graph.
+    The k-Delaunay graph (a.k.a., the order k Delaunay graph)
+    contains an edge ij if and only if there exists a disk
+    containing i and j and at most k additional points. See
+    that the 0-Delaunay graph is the standard Delaunay graph.
+    Moreover, for any natural k, k-Delaunay subseteq (k+1)-
+    Delaunay.
+
     Args:
-        points (list): List (len n) of lists (len 2) of floats representing
-          coordinates.
-        order (int): The order (k) of the graph to be computed.
+        points (list):
+            List (len n) of lists (len 2) of floats representing
+            coordinates.
+        order (int):
+            The order (k) of the graph to be computed.
     """
     assert (order >= 0) and isinstance(order, int)
     # compute k_delaunay graph
-    # see that we consider 0-Delaunay = (the standard) Delaunay,
-    # whereas those who implemented OrderKDelaunay consider
-    # 1-Delaunay = Delaunay. As such, we increment the order to match the
-    # OrderKDelaunay software's convention.
+    # we use 0-Delaunay = Delaunay triangulation, whereas those
+    # who implemented OrderKDelaunay use 1-Delaunay = Delaunay.
+    # As such, we increment the order to match the OrderKDelaunay
+    # software's convention.
     k_delaunay = OrderKDelaunay(points, order + 1)
     k_delaunay_edges = []
 
@@ -429,7 +473,12 @@ def get_kdelaunay_graph(points, order):
         for pair in it.combinations(clique_idxs, 2):
             k_delaunay_edges.append(list(pair))
 
-    k_delaunay_edges = set([tuple(sorted(edge)) for edge in k_delaunay_edges])
+    k_delaunay_edges = set(
+        [
+            tuple(sorted(edge))
+            for edge in k_delaunay_edges
+        ]
+    )
 
     k_delaunay = nx.Graph()
     points = np.array(points)
@@ -441,6 +490,6 @@ def get_kdelaunay_graph(points, order):
         for (v, w) in k_delaunay_edges
     ]
     k_delaunay.add_weighted_edges_from(edges)
-    k_delaunay.graph['type']   = 'kdel'
+    k_delaunay.graph['type'] = 'kdel'
 
     return k_delaunay
